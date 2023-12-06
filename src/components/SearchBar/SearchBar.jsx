fb4b3399328621d9350cbf1b710390330f08513a
SvitLana1990 <svitlana112@outlook.es>
2023-12-06 11:11:14 +0100

    fix mentor

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,11 @@
 import { Input } from './SearchBar.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { filterContacts } from '../../redux/contactsSlice';
+import { selectFilter } from 'redux/selectors';
 
 export const SearchBar = () => {
   const dispatch = useDispatch();
-  const filterValue = useSelector(state => state.contacts.filter);
+  const filterValue = useSelector(selectFilter);
 
   const handleFilterChange = value => {
     dispatch(filterContacts(value));
